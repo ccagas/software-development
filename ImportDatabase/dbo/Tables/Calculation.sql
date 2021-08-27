59a34cb7bdb90f149f48a8171ede78f63a321a64
Candie Cagas <82238028+ccagas@users.noreply.github.com>
2021-08-27 16:09:34 -0500

    Add files via upload

---
+++ ImportDatabase/dbo/Tables/Calculation.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Calculation] (
+    [CalculationId]  INT        NOT NULL,
+    [StudentId]      INT        NOT NULL,
+    [IsCorrect]      BIT        NULL,
+    [Operation]      NCHAR (10) NULL,
+    [Number1]        NCHAR (10) NULL,
+    [Number2]        NCHAR (10) NULL,
+    [ExpectedResult] NCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([CalculationId] ASC),
+    CONSTRAINT [FK_Calculation_Student] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([StudentId])
+);
+
---
+++ ImportDatabase/dbo/Tables/Student.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [StudentId] INT           NOT NULL,
+    [Name]      NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([StudentId] ASC)
+);
+
