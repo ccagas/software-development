da545804007067ab28f759612be9cd3eecca8665
Candie Cagas <82238028+ccagas@users.noreply.github.com>
2021-08-27 16:02:08 -0500

    Add files via upload

---
+++ ConsoleStartNumberAdder/ConsoleStartNumberAdder/Module1.vb
@@ -0,0 +1,51 @@
+﻿Imports Object_Oriented_Programming_Assignment
+Module Module1
+
+    Sub Main()
+        Dim currentStudent As Student
+        Dim firstNum As InputNumber
+        Dim secondNum As InputNumber
+        Dim expectedResult As InputNumber
+        Dim calculation As Calculation
+        Dim result As String
+
+        Console.WriteLine("Assignment: Console Number Adder" + vbNewLine)
+        currentStudent = New Student
+
+        Do
+
+            Console.WriteLine("Enter First Number:")
+            firstNum = New InputNumber
+            firstNum.Enter(Console.ReadLine)
+
+            Console.WriteLine("Enter Second Number:")
+            secondNum = New InputNumber
+            secondNum.Enter(Console.ReadLine)
+
+            Console.WriteLine("Enter Expected Result:")
+            expectedResult = New InputNumber
+            expectedResult.Enter(Console.ReadLine)
+
+            Console.WriteLine("Enter Operation:")
+            calculation = New Calculation
+            calculation.Operation = (Console.ReadLine)
+
+            calculation.Number1 = firstNum
+            calculation.Number2 = secondNum
+            calculation.ExpectedResult = expectedResult
+
+            currentStudent.CheckCalculation(calculation)
+            result = CType(calculation.IsCorrect, String)
+
+            Console.Write("Your answer is " + result + vbNewLine)
+            Console.WriteLine("Enter again? Y/N:")
+
+            If Console.ReadLine.ToUpper = "N" Then
+                Exit Do
+            End If
+
+        Loop
+
+    End Sub
+
+End Module
---
+++ ConsoleStartNumberAdder/ConsoleStartNumberAdder/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleStartNumberAdder")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ConsoleStartNumberAdder")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e34a54d3-ffc8-4d85-925e-72af78b734b4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
