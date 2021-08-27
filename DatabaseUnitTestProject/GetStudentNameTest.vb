2db6e65d1aa1f08a178cc9dff7b5d32758b391dc
Candie Cagas <82238028+ccagas@users.noreply.github.com>
2021-08-27 16:07:22 -0500

    Add files via upload

---
+++ DatabaseUnitTestProject/GetStudentNameTest.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class GetStudentNameTest
+
+    <TestMethod()> Public Sub GetStudentName()
+        Dim studentName As New StudentCalculationsEntities
+        Dim studentCount As Integer = 0
+
+        For Each student As Student In studentName.Students
+            Debug.Print("", studentName)
+        Next
+
+        studentCount += 1
+    End Sub
+
+End Class
---
+++ DatabaseUnitTestProject/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("DatabaseUnitTestProject")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("DatabaseUnitTestProject")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("99c630de-44ae-4069-abd8-94330c4a0d38")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
